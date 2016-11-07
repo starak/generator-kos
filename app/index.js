97402453d5e57704bb4fed2e6893e98d6eac07a0
Ståle Raknes <stale.raknes@knowit.no>
2016-11-07 17:19:27 +0100

    Initial commit. Introduced page generator along side component generator. Started on sass introduction.

--- app/index.js
+++ app/index.js
@@ -1,103 +1,104 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
+var util = require( 'util' );
+var path = require( 'path' );
+var yeoman = require( 'yeoman-generator' );
+var chalk = require( 'chalk' );
 
-var KoGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+var KosGenerator = yeoman.generators.Base.extend( {
+    init: function () {
+        this.pkg = require( '../package.json' );
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        // Figure out whether we have an internet connection. If not, need to
-        // pass --offline to bower otherwise it won't fall back on cache.
-        require('dns').resolve('example.com', function(isOffline) {
-          console.log('Installing dependencies in ' + (isOffline ? 'offline' : 'online') + ' mode...');
-          if (isOffline) {
-            // Patch bowerInstall to pass --offline
-            this.bowerInstall = (function(originalFunction) {
-              return function(paths, options, cb) {
-                options = options || {};
-                options.offline = true;
-                return originalFunction.call(this, paths, options, cb);
-              };
-            })(this.bowerInstall);
-          }
+        this.on( 'end', function () {
+            if ( !this.options['skip-install'] ) {
+                // Figure out whether we have an internet connection. If not, need to
+                // pass --offline to bower otherwise it won't fall back on cache.
+                require( 'dns' ).resolve( 'example.com', function ( isOffline ) {
+                    console.log( 'Installing dependencies in ' + (isOffline ? 'offline' : 'online') + ' mode...' );
+                    if ( isOffline ) {
+                        // Patch bowerInstall to pass --offline
+                        this.bowerInstall = (function ( originalFunction ) {
+                            return function ( paths, options, cb ) {
+                                options = options || {};
+                                options.offline = true;
+                                return originalFunction.call( this, paths, options, cb );
+                            };
+                        })( this.bowerInstall );
+                    }
 
-          this.installDependencies();
+                    this.installDependencies();
 
-          if (this.includeTests) {
-            // Install test dependencies too
-            var bowerArgs = ['install'];
-            if (isOffline) {
-              bowerArgs.push('--offline');
+                    if ( this.includeTests ) {
+                        // Install test dependencies too
+                        var bowerArgs = ['install'];
+                        if ( isOffline ) {
+                            bowerArgs.push( '--offline' );
+                        }
+                        this.spawnCommand( 'bower', bowerArgs, {cwd: 'test'} );
+                    }
+                }.bind( this ) );
             }
-            this.spawnCommand('bower', bowerArgs, { cwd: 'test' });
-          }
-        }.bind(this));
-      }
-    });
-  },
+        } );
+    },
 
-  askFor: function () {
-    var done = this.async();
-    this.log(this.yeoman);
-    this.log(chalk.magenta('You\'re using the fantastic Knockout app generator.'));
+    askFor: function () {
+        var done = this.async();
+        this.log( this.yeoman );
+        this.log( chalk.magenta( 'You\'re using the fantastic Knockout app generator.' ) );
 
-    var prompts = [{
-      name: 'name',
-      message: 'What\'s the name of your new site?',
-      default: path.basename(process.cwd())
-    }, {
-      type: 'confirm',
-      name: 'includeTests',
-      message: 'Do you want to include automated tests, using Jasmine and Karma?',
-      default: true
-    }];
+        var prompts = [{
+            name: 'name',
+            message: 'What\'s the name of your new site?',
+            default: path.basename( process.cwd() )
+        }, {
+            type: 'confirm',
+            name: 'includeTests',
+            message: 'Do you want to include automated tests, using Jasmine and Karma?',
+            default: false
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.longName = props.name;
-      this.slugName = this._.slugify(this.longName);
-      this.includeTests = props.includeTests;
-      done();
-    }.bind(this));
-  },
+        this.prompt( prompts, function ( props ) {
+            this.longName = props.name;
+            this.slugName = this._.slugify( this.longName );
+            this.includeTests = props.includeTests;
+            done();
+        }.bind( this ) );
+    },
 
-  templating: function () {
-    this._processDirectory('src', 'src');
-    this.template('_package.json', 'package.json');
-    this.template('_bower.json', 'bower.json');
-    this.template('_gulpfile.js', 'gulpfile.js');
-    this.template('_gitignore', '.gitignore');
-    this.copy('bowerrc', '.bowerrc');
-    this.copy('jsconfig.json');
+    templating: function () {
+        this._processDirectory( 'src', 'src' );
+        this.template( '_package.json', 'package.json' );
+        this.template( '_bower.json', 'bower.json' );
+        this.template( '_gulpfile.js', 'gulpfile.js' );
+        this.template( '_gitignore', '.gitignore' );
+        this.copy( 'bowerrc', '.bowerrc' );
+        this.copy( 'jsconfig.json' );
 
-    if (this.includeTests) {
-      // Set up tests
-      this._processDirectory('test', 'test');
-      this.copy('bowerrc_test', 'test/.bowerrc');
-      this.copy('karma.conf.js');
-    }
-  },
-
-  _processDirectory: function(source, destination) {
-    var root = this.isPathAbsolute(source) ? source : path.join(this.sourceRoot(), source);
-    var files = this.expandFiles('**', { dot: true, cwd: root });
-
-    for (var i = 0; i < files.length; i++) {
-        var f = files[i];
-        var src = path.join(root, f);
-        if(path.basename(f).indexOf('_') == 0){
-            var dest = path.join(destination, path.dirname(f), path.basename(f).replace(/^_/, ''));
-            this.template(src, dest);
+        if ( this.includeTests ) {
+            // Set up tests
+            this._processDirectory( 'test', 'test' );
+            this.copy( 'bowerrc_test', 'test/.bowerrc' );
+            this.copy( 'karma.conf.js' );
         }
-        else{
-            var dest = path.join(destination, f);
-            this.copy(src, dest);
+    },
+
+    _processDirectory: function ( source, destination ) {
+        var root = this.isPathAbsolute( source ) ? source : path.join( this.sourceRoot(), source );
+        var files = this.expandFiles( '**', {dot: true, cwd: root} );
+        var dest;
+
+        for ( var i = 0; i < files.length; i++ ) {
+            var f = files[i];
+            var src = path.join( root, f );
+            if ( path.basename( f ).indexOf( '_' ) == 0 ) {
+                dest = path.join( destination, path.dirname( f ), path.basename( f ).replace( /^_/, '' ) );
+                this.template( src, dest );
+            }
+            else {
+                dest = path.join( destination, f );
+                this.copy( src, dest );
+            }
         }
     }
-  }
-});
+} );
 
-module.exports = KoGenerator;
+module.exports = KosGenerator;
---
+++ page/templates/viewmodel.js
@@ -0,0 +1,15 @@
+import ko from 'knockout';
+import templateMarkup from 'text!./<%= filename %>.html';
+
+class <%= viewModelClassName %> {
+    constructor(params) {
+        this.message = ko.observable('Hello from the <%= name %> page!');
+    }
+    
+    dispose() {
+        // This runs when the component is torn down. Put here any logic necessary to clean up,
+        // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
+    }
+}
+
+export default { viewModel: <%= viewModelClassName %>, template: templateMarkup };
